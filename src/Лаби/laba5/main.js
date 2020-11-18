e021750d45f90c4beac0046e14706cbbdf382306
petro <gapierre4@gmail.com>
2020-11-18 11:25:09 +0200

    Лаба5

---
+++ src/Лаби/laba5/main.js
@@ -0,0 +1,31 @@
+$( function() {
+    $( "#tabs").tabs();
+} );
+$( function() {
+    $( "#datepicker" ).datepicker();
+} );
+$( function() {
+    $( "#accordion" ).accordion();
+} );
+$( function() {
+    $( "#slider-range" ).slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [ 75, 300 ],
+        slide: function( event, ui ) {
+            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+        " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+} );
+$( function() {
+    $( "#menu" ).menu();
+} );
+$( function() {
+    $( "#dialog" ).dialog();
+} );
+jQuery(document).ready(function(){
+    jQuery("#gallery").unitegallery();
+});
